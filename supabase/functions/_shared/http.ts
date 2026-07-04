e0565170849a740c2bd28c8720bd46a19ed67143
yueshan <yueshan.chao@quvideo.com>
2026-07-04 10:23:29 +0800

    逻辑修改

--- supabase/functions/_shared/http.ts
+++ supabase/functions/_shared/http.ts
@@ -23,12 +23,15 @@ function getConfiguredAllowedOrigins() {
 }
 
 function isAllowedChromeExtensionOrigin(origin: string) {
-  try {
-    const url = new URL(origin);
-    return url.protocol === "chrome-extension:" && /^[a-p]{32}$/.test(url.hostname);
-  } catch (_error) {
-    return false;
+  // Support Chrome/Edge extensions: chrome-extension://[32-letter-id]
+  if (/^chrome-extension:\/\/([a-p]{32})\/?$/.test(origin)) {
+    return true;
   }
+  // Support Firefox & Safari extensions: moz-extension://[uuid] or safari-web-extension://[uuid]
+  if (/^(moz|safari-web)-extension:\/\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\/?$/i.test(origin)) {
+    return true;
+  }
+  return false;
 }
 
 export function isAllowedBrowserOrigin(request: Request) {
